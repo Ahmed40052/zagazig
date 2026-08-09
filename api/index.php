f963f95fa9799357edcb40ad317accf996e69ba0
Ahmed Ayman <ahmeddayman78@gmail.com>
2026-08-09 20:26:24 +0300

    Fix Laravel writable paths on Vercel

--- api/index.php
+++ api/index.php
@@ -1,38 +1,13 @@
 <?php
 
-echo "1 - PHP works<br>";
-
 require __DIR__ . '/../vendor/autoload.php';
-
-echo "2 - Autoload works<br>";
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-echo "3 - Laravel bootstrap works<br>";
-
 $request = Illuminate\Http\Request::capture();
 
-echo "4 - Request works<br>";
+$response = $app->handleRequest($request);
 
-try {
-    $response = $app->handleRequest($request);
+$response->send();
 
-    echo "5 - Request handled<br>";
-
-    $response->send();
-
-    echo "6 - Response sent<br>";
-
-    $app->terminate();
-
-} catch (\Throwable $e) {
-
-    echo "<h2>ERROR:</h2>";
-
-    echo "<pre>";
-    echo "Message: " . $e->getMessage() . "\n\n";
-    echo "File: " . $e->getFile() . "\n";
-    echo "Line: " . $e->getLine() . "\n\n";
-    echo "Trace:\n" . $e->getTraceAsString();
-    echo "</pre>";
-}
+$app->terminate();
