6bdf3256719f6b5bb7295b3e02358c3229679614
Ahmed Ayman <ahmeddayman78@gmail.com>
2026-08-09 20:22:20 +0300

    Debug Laravel startup

--- api/index.php
+++ api/index.php
@@ -1,12 +1,22 @@
 <?php
 
+echo "1 - PHP works<br>";
+
 require __DIR__ . '/../vendor/autoload.php';
+
+echo "2 - Autoload works<br>";
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+echo "3 - Laravel bootstrap works<br>";
+
 $request = Illuminate\Http\Request::capture();
 
+echo "4 - Request works<br>";
+
 $response = $app->handleRequest($request);
+
+echo "5 - Laravel handled request<br>";
 
 $response->send();
 
